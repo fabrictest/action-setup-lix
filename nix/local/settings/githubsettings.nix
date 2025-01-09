bf1a5e8d8137f5de79589000ddfbd865daca067f
F. Emerson <t@utologi.cc>
2025-01-08 22:45:59 -0300

    chore: update repository settings

--- nix/local/settings/githubsettings.nix
+++ nix/local/settings/githubsettings.nix
@@ -1,118 +1,133 @@
 # https://github.com/apps/settings
 let
-  inherit (inputs) std;
+  inherit (inputs) l std;
 in
 std.lib.dev.mkNixago std.lib.cfg.githubsettings {
   data = {
 
-    # bypass_actors = [ ];
-
-    repository =
-      let
-        name = "action-setup-lix";
-      in
-      {
-        inherit name;
-        description = "Install Lix faster than you can refresh a GitHub Actions workflow page";
-        homepage = "https://github.com/fabrictest/${name}";
-        topics = "github-actions,lix,nix";
-        visibility = "public";
-        security_and_analysis = null;
-        has_issues = true;
-        has_projects = false;
-        has_wiki = false;
-        is_template = false;
-        default_branch = "main";
-        allow_squash_merge = true;
-        allow_merge_commit = false;
-        allow_rebase_merge = false;
-        allow_auto_merge = true;
-        delete_branch_on_merge = true;
-        allow_update_branch = true;
-        squash_merge_commit_title = "PR_TITLE";
-        squash_merge_commit_message = "PR_BODY";
-        merge_commit_title = "PR_TITLE";
-        merge_commit_message = "PR_BODY";
-        enable_automated_security_fixes = true;
-        enable_vulnerability_alerts = true;
-      };
+    repository = {
+      name = "action-setup-lix";
+      description = "Install Lix faster than you can refresh a GitHub Actions workflow page";
+      homepage = "https://github.com/fabrictest/action-setup-lix";
+      topics = "github-actions,lix,nix";
+      visibility = "public";
+      security_and_analysis = null;
+      has_issues = true;
+      has_projects = false;
+      has_wiki = false;
+      is_template = false;
+      default_branch = "main";
+      allow_squash_merge = true;
+      allow_merge_commit = false;
+      allow_rebase_merge = false;
+      allow_auto_merge = true;
+      delete_branch_on_merge = true;
+      allow_update_branch = true;
+      squash_merge_commit_title = "PR_TITLE";
+      squash_merge_commit_message = "PR_BODY";
+      merge_commit_title = "PR_TITLE";
+      merge_commit_message = "PR_BODY";
+      enable_automated_security_fixes = true;
+      enable_vulnerability_alerts = true;
+    };
 
     # labels = [ ];
 
     rulesets = [
       {
-        name = "Prevent tampering with the default branch";
         target = "branch";
+        name = "Prevent tampering with default branch";
         enforcement = "active";
-        conditions = {
-          ref_name = {
-            include = [ "~DEFAULT_BRANCH" ];
-            exclude = [];
-          };
-        };
+        conditions.ref_name.include = [ "~DEFAULT_BRANCH" ];
         rules = [
           { type = "deletion"; }
-          # { type = "non_fast_forward"; }
-            { type = "required_linear_history"; }
-            {
-              type = "pull_request";
-              parameters = {
-                require_code_owner_review = true;
-              };
-            }
+          { type = "non_fast_forward"; }
+          { type = "required_linear_history"; }
         ];
       }
-      /*
-        {
-            {
-              type = "pull_request";
-              parameters = {
-                allowed_merge_methods = [ "SQUASH" ];
-                dismiss_stale_reviews_on_push = true;
-                require_code_owner_review = true;
-                require_last_pull_approval = true;
-                required_approving_review_count = 1;
-                required_review_thread_resolution = true;
-              };
-            }
-              {
-                type = "required_status_checks";
-                parameters = {
-                  required_status_checks = [
-                    # TODO(ttlgcc): Gather integration IDs and configure the required status checks.
-                    #  See: https://docs.github.com/en/rest/repos/rules?apiVersion=2022-11-28#update-a-repository-ruleset
-                  ];
-                  strict_required_status_checks_policy = true;
-                };
-              }
-          ];
-        }
-
-        {
-          name = "Prevent tampering with the release tags";
-          target = "tag";
-          enforcement = "enabled";
-          conditions = {
-            ref_name = {
-              include = [ "~ALL" ];
-              exclude = [];
+      {
+        target = "branch";
+        name = "Require PRs to pass the quality bar";
+        enforcement = "active";
+        conditions.ref_name.include = [ "~DEFAULT_BRANCH" ];
+        rules = [
+          {
+            type = "merge_queue";
+            parameters = {
+              merge_method = "SQUASH";
+              max_entries_to_build = 5;
+              min_entries_to_merge = 1;
+              max_entries_to_merge = 5;
+              min_entries_to_merge_wait_minutes = 5;
+              grouping_strategy = "ALLGREEN";
+              check_response_timeout_minutes = 60;
             };
-          };
-          rules = [
-            { type = "deletion"; }
-            { type = "non_fast_forward"; }
-            {
-              type = "tag_name_pattern";
-              parameters = {
-                name = "Tags follow the SemVer convention";
-                negate = false;
-                operator = "regex";
-                pattern = ''^v?(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$'';
-              };
-            }
-          ];
-        }
-      */
+          }
+          {
+            type = "pull_request";
+            parameters = {
+              required_approving_review_count = 1;
+              dismiss_stale_reviews_on_push = true;
+              require_code_owner_review = true;
+              require_last_push_approval = true;
+              required_review_thread_resolution = true;
+              automatic_copilot_code_review_enabled = true;
+              allowed_merge_methods = [ "squash" ];
+            };
+          }
+          {
+            type = "required_status_checks";
+            parameters = {
+              strict_required_status_checks_policy = false;
+              required_status_checks =
+                let
+                  context = l.cartesianProduct {
+                    workflow = [
+                      "Test"
+                    ];
+                    job = [
+                      "Examples"
+                      "With Cachix"
+                    ];
+                    lix-version = [
+                      "2.90.0"
+                      "2.91.1"
+                    ]; # TODO(ttlgcc): Get versions from `lix` cell.
+                    runs-on = [
+                      "macos-13"
+                      "macos-15"
+                      "ubuntu-24.04"
+                    ];
+                  };
+                in
+                l.cartesianProduct {
+                  context = l.map (c: "${c.workflow} / ${c.job} (${c.lix-version}, ${c.runs-on})") context;
+                  integration_id = [ 15368 ]; # GitHub Actions
+                };
+            };
+          }
+        ];
+      }
+
+      {
+        target = "tag";
+        name = "Prevent deletion of release tags";
+        enforcement = "active";
+        conditions.ref_name.include = [ "refs/tags/v*" ];
+        rules = [
+          { type = "deletion"; }
+        ];
+      }
+      {
+        target = "tag";
+        name = "Prevent tampering with release tags";
+        enforcement = "active";
+        conditions.ref_name.include = [ "refs/tags/v*.*.*" ];
+        rules = [
+          { type = "non_fast_forward"; }
+          { type = "update"; }
+        ];
+      }
     ];
   };
 }
